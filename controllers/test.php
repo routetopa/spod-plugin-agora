1f624fa742e977f8abf5edf55161e34ddf1a610e
isisadmin <isislab.unisa@gmail.com>
2015-11-06 09:35:59 +0100

    public room

---
+++ controllers/test.php
@@ -0,0 +1,10 @@
+<?php
+
+class SPODPUBLIC_CTRL_Test extends OW_ActionController
+{
+    public function index()
+    {
+
+    }
+
+}
---
+++ init.php
@@ -0,0 +1,3 @@
+<?php
+
+OW::getRouter()->addRoute(new OW_Route('spodpublic.test', 'public-room/test', "SPODPUBLIC_CTRL_Test", 'index'));
