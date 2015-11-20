ad4d1d3953d883072d36b93d5b1e20d5cb4dbf05
luigser <luigser@gmail.com>
2015-11-20 12:36:38 +0100

    public room graph updates

--- classes/graph.php
+++ classes/graph.php
@@ -38,43 +38,49 @@ class SPODPUBLIC_CLASS_Graph
 
             $this->normalizedNodeIds[$node->originalId] = 0;
 
-            $this->analyzeCommentsTree(BOL_CommentService::getInstance()->findFullCommentList(SPODPUBLIC_BOL_Service::ENTITY_TYPE, $id), 0, $type);
+            //$this->analyzeCommentsTree(BOL_CommentService::getInstance()->findFullCommentList(SPODPUBLIC_BOL_Service::ENTITY_TYPE, $id), 0, $type);
+
+            switch($type)
+            {
+                case "comment":
+                    $this->getCommentsGraph(BOL_CommentService::getInstance()->findFullCommentList(SPODPUBLIC_BOL_Service::ENTITY_TYPE, $id), 0);
+                    break;
+                case "datalet":
+                    $this->getDataletsGraph(BOL_CommentService::getInstance()->findFullCommentList(SPODPUBLIC_BOL_Service::ENTITY_TYPE, $id), $node, 0);
+                    break;
+            }
 
             return $this->graph;
 
     }
 
-    private function analyzeCommentsTree($nodes, $level, $type)
+    private function getDataletsGraph($nodes, $father, $level)
     {
+        $curr_father = $father;
 
         for ($i = 0; $i < count($nodes); $i++)
         {
-            $node = null;
-            if ($type == "comment") {
-                $node = new Node(count($this->graph->nodes),
-                    BOL_UserService::getInstance()->getDisplayName($nodes[$i]->userId),
-                    intval($nodes[$i]->id));
-
-            }else {
-                if (OW::getPluginManager()->isPluginActive('spodpr')) {
-                    $datalet = ODE_BOL_Service::getInstance()->getDataletByPostId($nodes[$i]->id, "public-room");
-                    if (count($datalet) > 0) {
-                        $node = new Node(count($this->graph->nodes),
-                            $datalet["component"],
-                            intval($nodes[$i]->id));
-                    }
+            if (OW::getPluginManager()->isPluginActive('spodpr')) {
+                $datalet = ODE_BOL_Service::getInstance()->getDataletByPostId($nodes[$i]->id, "public-room");
+                if (count($datalet) > 0) {
+                    $node = new Node(count($this->graph->nodes),
+                        $datalet["component"],
+                        intval($nodes[$i]->id));
+                    $node->type = "datalet";
+                }else{
+                    $node = new Node(count($this->graph->nodes),
+                        "",
+                        intval($nodes[$i]->id));
+                    $node->type = "comment";
                 }
+            }else{
+                //ODE plugin is not active then the datalets graph cannot be created
+                return;
             }
 
-            if($node == null){
-                $node = new Node(count($this->graph->nodes),
-                    "",
-                    intval($nodes[$i]->id));
 
-                $node->color = "#000000";
-                $node->r = 1;
+            if($node->type == "datalet"){
 
-            }else {
                 $node->content = $nodes[$i]->message;
                 switch ($level) {
                     case 0:
@@ -87,9 +93,56 @@ class SPODPUBLIC_CLASS_Graph
                         break;
                     case 2:
                         $node->color = "#a7a1a1";
-                        $node->r = 5;
+                        $node->r = 10;
                         break;
                 }
+
+                array_push($this->graph->nodes, $node);
+
+                $link = new Link($father->id, intval($node->id));
+
+                switch ($level) {
+                    case 0:
+                        $link->value = 70;
+                        break;
+                    case 1:
+                        $link->value = 50;
+                        break;
+                    case 2:
+                        $link->value = 30;
+                        break;
+                }
+
+                array_push($this->graph->links, $link);
+
+                $curr_father = $node;
+            }
+
+            $this->getDataletsGraph(BOL_CommentService::getInstance()->findFullCommentList(SPODPUBLIC_BOL_Service::ENTITY_TYPE, $nodes[$i]->id), $curr_father, $level + 1);
+        }
+    }
+
+    private function getCommentsGraph($nodes ,$level)
+    {
+        for ($i = 0; $i < count($nodes); $i++) {
+            $node = new Node(count($this->graph->nodes),
+                BOL_UserService::getInstance()->getDisplayName($nodes[$i]->userId),
+                intval($nodes[$i]->id));
+
+            $node->content = $nodes[$i]->message;
+            switch ($level) {
+                case 0:
+                    $node->color = "#ff1e1e";
+                    $node->r = 20;
+                    break;
+                case 1:
+                    $node->color = "#3399cc";
+                    $node->r = 15;
+                    break;
+                case 2:
+                    $node->color = "#a7a1a1";
+                    $node->r = 5;
+                    break;
             }
 
             array_push($this->graph->nodes, $node);
@@ -111,22 +164,8 @@ class SPODPUBLIC_CLASS_Graph
 
             array_push($this->graph->links, $link);
 
-
-            $this->analyzeCommentsTree(BOL_CommentService::getInstance()->findFullCommentList(SPODPUBLIC_BOL_Service::ENTITY_TYPE, $nodes[$i]->id), $level + 1, $type);
+            $this->getCommentsGraph(BOL_CommentService::getInstance()->findFullCommentList(SPODPUBLIC_BOL_Service::ENTITY_TYPE, $nodes[$i]->id), $level + 1);
         }
-    }
-
-    public function getRoot($id){
-        $comment = BOL_CommentService::getInstance()->findComment($id);
-        if($comment == null) return $id;
-        $root = null;
-        while($comment)
-        {
-            $entity = BOL_CommentEntityDao::getInstance()->findById($comment->getCommentEntityId());
-            $root = $entity->entityId;
-            $comment = BOL_CommentService::getInstance()->findComment($entity->entityId);
-        }
-        return $root;
     }
 }
 
@@ -139,6 +178,7 @@ class Node{
     public $content;
     public $color;
     public $r;
+    public $type;
 
     function __construct($id, $name, $originalId){
         $this->id         = $id;
