932a22e1c926c934630d1f5b151bbddbff0f2ccc
andrpet <andrpet@gmail.com>
2016-09-30 16:58:06 +0200

    agora language update and cocreation sava metadata button position fixed

--- activate.php
+++ activate.php
@@ -3,3 +3,6 @@
     $authorization = OW::getAuthorization();
     $groupName = 'spodpublic';
     $authorization->addAction($groupName, 'create_room');
+
+    BOL_LanguageService::getInstance()->importPrefixFromZip(OW::getPluginManager()->getPlugin('spodpublic')->getRootDir() . 'langs.zip', 'spodpublic');
+
--- deactivate.php
+++ deactivate.php
@@ -1,1 +1,3 @@
 <?php
+
+BOL_LanguageService::getInstance()->deletePrefix(BOL_LanguageService::getInstance()->findPrefixId('spodpublic'));
